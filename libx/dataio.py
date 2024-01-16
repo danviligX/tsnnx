1e2f431107f6547a79ac0f9209304ac5a17486f6
danvilig <danvilig@outlook.com>
2024-01-16 22:14:41 +0800

    AniTracks

--- libx/dataio.py
+++ libx/dataio.py
@@ -4,6 +4,8 @@ import sys
 from tqdm import trange
 import numpy as np
 from torch.utils.data import Dataset,DataLoader
+from matplotlib import pyplot as plt
+from matplotlib import animation
 
 class Args(object):
     def __init__(self) -> None:
@@ -90,7 +92,61 @@ class Dset(object):
         for car in self.set[:,:,0].unique():
             if car in cid: continue
             else: self.set[:,car.int(),:] = torch.zeros_like(self.set[:,car.int(),:])
-        
+
+class AniTrack():
+    def __init__(self) -> None:
+        self.Tracks = None
+        self.ani = None
+        self.p_ani = None
+
+    def clear(self):
+        self.Tracks = None
+        self.ani = None
+        self.p_ani = None
+
+    def load(self,Tracks):
+        self.Tracks = Tracks
+    
+    def init(self,figsize=(20,6)):
+        self.fig = plt.figure(figsize=figsize)
+
+        for i in range(self.Tracks.size(1)):
+            x = self.Tracks[:,i,1]
+            y = self.Tracks[:,i,2]
+
+            plt_x = x[x!=0]
+            plt_y = y[y!=0]
+
+            self.line = plt.plot(plt_x,plt_y,'#1f77b4')
+
+        x = self.Tracks[0,:,1]
+        y = self.Tracks[0,:,2]
+
+        init_x = x[x!=0]
+        init_y = y[y!=0]
+
+        self.p_ani = plt.plot(init_x,init_y,'ro')
+        plt.grid(ls='--')
+
+    def update_p(self,flid):
+        x = self.Tracks[flid,:,1]
+        y = self.Tracks[flid,:,2]
+
+        fresh_x = x[x!=0]
+        fresh_y = y[y!=0]
+        self.p_ani[0].set_data(fresh_x, fresh_y)
+
+        return self.p_ani
+
+    def ani_play(self,frames=np.arange(0, 200),interval=30):
+        if self.p_ani == None: self.init()
+
+        self.ani = animation.FuncAnimation(self.fig, self.update_p,
+                                        frames = frames,
+                                        interval=interval,
+                                        blit=True)
+        plt.show()
+
 class vtp_dataset(Dataset):
     def __init__(self,use_index) -> None:
         super().__init__()
